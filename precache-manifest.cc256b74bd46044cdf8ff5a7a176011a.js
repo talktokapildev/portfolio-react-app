20185f274bb64188185f8e047f9c7e058b1d3b78
Kapil Dev <talktokapildev@gmail.com>
2019-12-18 07:15:58 +0000

    Updates

---
+++ precache-manifest.cc256b74bd46044cdf8ff5a7a176011a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3c014e9c6415fc0e60d573e18b8f6a85",
+    "url": "/portfolio-react-app/index.html"
+  },
+  {
+    "revision": "f61a7ac6eccbf031b8ca",
+    "url": "/portfolio-react-app/static/css/main.28b97720.chunk.css"
+  },
+  {
+    "revision": "9533b0a1182a0f697161",
+    "url": "/portfolio-react-app/static/js/2.780cfbb5.chunk.js"
+  },
+  {
+    "revision": "f05bfc683a175306602e6f609a238cdb",
+    "url": "/portfolio-react-app/static/js/2.780cfbb5.chunk.js.LICENSE"
+  },
+  {
+    "revision": "f61a7ac6eccbf031b8ca",
+    "url": "/portfolio-react-app/static/js/main.9af9a3ad.chunk.js"
+  },
+  {
+    "revision": "8f47f13551c3daa68766",
+    "url": "/portfolio-react-app/static/js/runtime-main.341b458a.js"
+  }
+]);
